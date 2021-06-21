81af6ccda16a9e294f7ea908072366d6f9665433
Kingjizrw <512264319@qq.com>
2021-06-21 22:02:07 +0800

    Update selection.php
    selection test(php)

--- selection.php
+++ selection.php
@@ -26,11 +26,32 @@
 	}
 	echo "连接成功<br>";
 	
-	$sql = "SELECT content FROM question";
-	$result = $conn->query($sql);
-	if($result->num_rows > 0){
-		while($row = $result->fetch_assoc()){
-			echo $row["content"] . "<br>";
+	$title_sql = "SELECT content FROM question;";
+	$title_result = $conn->query($title_sql);
+	$cnt = 0;
+	if($title_result->num_rows > 0){
+		while($title_row = $title_result->fetch_assoc()){
+			++$cnt;
+			echo (string)$cnt . ". " . $title_row["content"] . "<br>";
+			$pconn = new mysqli($servername,$username,$password,$databasename);
+			if($pconn->connect_error){
+				echo "连接失败";
+				break;
+			}
+			else{
+				$selection_sql = (string)"SELECT optionidinquestion,content FROM options where questionid=".(string)$cnt." order by optionidinquestion;";
+				//echo $selection_sql . "<br>";
+				$selection_result = $pconn->query($selection_sql);
+				if($selection_result->num_rows > 0){
+					while($selection_row = $selection_result->fetch_assoc()){
+						echo "&nbsp;&nbsp;&nbsp;&nbsp;".$selection_row["optionidinquestion"].". ".$selection_row["content"]."<br>";
+						
+					}
+				}
+				echo "<br>";
+			}
+			
+			
 		}
 	}
 	
